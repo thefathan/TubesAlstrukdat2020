b09735b8fac2bf030cd9d324d1f0c3efb3e78f03
NaufalAnas <naufalanas46@gmail.com>
2020-12-05 19:39:08 +0700

    file array.c update

--- ADT/array/array.c
+++ ADT/array/array.c
@@ -1,316 +1,146 @@
-/* NAMA / NIM :                           */
-/* 1. Muhammad Hasan           / 13518012 */
-/* 2. Anna Elvira Hartoyo      / 13518045 */
-/* 3. Daniel Riyanto           / 13518075 */
-/* 4. Faris Muhammad Kautsar   / 13518105 */
-/* 5. Gregorius Jovan Kresnadi / 13518135 */
-
-/* File body: array.c */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "array.h"
+#include "../array/array.h"
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create tabel kosong  */
-void MakeEmptyBangunan (Bangunan * B, int maxel) {
-  /* KAMUS LOKAL */
-  
-  /* ALGORITMA */
-  if (maxel > 0) {
-    BI(*B) = (info_bangunan *) malloc ((maxel + 1)* sizeof(info_bangunan));
-    MaxEl(*B) = maxel;
-    Neff(*B) = 0;
-  }
+void MakeEmpty(TabInt *T, int maxel){
+    TI(*T) = (ElType *) malloc (sizeof(ElType) * (maxel+1));
+    Neff(*T) = 0;
+    MaxEl(*T) = maxel;
 }
+/* I.S. T sembarang, maxel > 0 */
+/* F.S. Terbentuk tabel T kosong dengan kapasitas maxel + 1 */
 
-void DealokasiBangunan(Bangunan *B) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  free(BI(*B));
-  MaxEl(*B) = 0;
-  Neff(*B) = 0;
+void Dealokasi(TabInt *T){
+    free(TI(*T));
+    MaxEl(*T) = 0;
+    Neff(*T) = 0;
 }
+/* I.S. T terdefinisi; */
+/* F.S. TI(T) dikembalikan ke system, MaxEl(T)=0; Neff(T)=0 */
 
 /* ********** SELEKTOR (TAMBAHAN) ********** */
 /* *** Banyaknya elemen *** */
-int NbElmtBan (Bangunan B) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  return Neff(B);
+int NbElmtTab(TabInt T){
+    return Neff(T);
 }
-
+/* Mengirimkan banyaknya elemen efektif tabel */
+/* Mengirimkan nol jika tabel kosong */
+/* *** Daya tampung container *** */
+int MaxElement(TabInt T){
+    return (MaxEl(T));
+}
+/* Mengirimkan maksimum elemen yang dapat ditampung oleh tabel */
 /* *** Selektor INDEKS *** */
-IdxType GetFirstBan (Bangunan B) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  return IdxMin;
+IdxType GetFirstIdx(TabInt T){
+    return (IdxMin);
 }
-
-IdxType GetLastBan (Bangunan B) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  return Neff(B);
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen T pertama */
+IdxType GetLastIdx(TabInt T){
+    return (Neff(T));
 }
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen T terakhir */
 
-int GetMaxTentara(Bangunan B, IdxType X) {
-	if (Name(ElmtBan(B, X)) == 'C') {
-		if (Level(ElmtBan(B, X)) == 1) {
-		return MaxC1;
-
-		} else if (Level(ElmtBan(B, X)) == 2) {
-		return MaxC2;
-
-		} else if (Level(ElmtBan(B, X)) == 3) {
-		return MaxC3;
-
-		} else /* Level(ElmtBan(B, X)) == 4 */ {
-		return MaxC4;
-
-		}
-	} else if (Name(ElmtBan(B, X)) == 'T') {
-		if (Level(ElmtBan(B, X)) == 1) {
-		return MaxT1;
-
-		} else if (Level(ElmtBan(B, X)) == 2) {
-		return MaxT2;
-
-		} else if (Level(ElmtBan(B, X)) == 3) {
-		return MaxT3;
-
-		} else /* Level(ElmtBan(B, X)) == 4 */ {
-		return MaxT4;
-
-		}
-	} else if (Name(ElmtBan(B, X)) == 'F') {
-		if (Level(ElmtBan(B, X)) == 1) {
-		return MaxF1;
-
-		} else if (Level(ElmtBan(B, X)) == 2) {
-		return MaxF2;
-
-		} else if (Level(ElmtBan(B, X)) == 3) {
-		return MaxF3;
-
-		} else /* Level(ElmtBan(B, X)) == 4 */ {
-		return MaxF4;
-
-		}
-	} else /* Name(ElmtBan(B, X)) == 'V' */ {
-		if (Level(ElmtBan(B, X)) == 1) {
-		return MaxV1;
-
-		} else if (Level(ElmtBan(B, X)) == 2) {
-		return MaxV2;
-
-		} else if (Level(ElmtBan(B, X)) == 3) {
-		return MaxV3;
-
-		} else /* Level(ElmtBan(B, X)) == 4 */ {
-		return MaxV4;
-
-		}
-	}
+/* ********** Test Indeks yang valid ********** */
+boolean IsIdxValid(TabInt T, IdxType i){
+    if((i <= MaxEl(T)) && (i >= IdxMin)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+/* Mengirimkan true jika i adalah indeks yang valid utk ukuran tabel */
+/* yaitu antara indeks yang terdefinisi utk container*/
+boolean IsIdxEff(TabInt T, IdxType i){
+    if((i >= GetFirstIdx(T)) && (i <= GetLastIdx(T))){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel */
+/* yaitu antara FirstIdx(T)..LastIdx(T) */
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test tabel kosong *** */
-boolean IsEmptyBan (Bangunan B) {
-  return (Neff(B) == 0);
+boolean IsEmpty(TabInt T){
+    return (Neff(T) == 0);
 }
-
+/* Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak */
 /* *** Test tabel penuh *** */
-boolean IsFullBan (Bangunan B) {
-// ? Entah perlu fungsi ini gak ya?
-  return (Neff(B) == MaxEl(B));
+boolean IsFull(TabInt T){
+    return (Neff(T) == MaxEl(T));
 }
-
-/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
-
-//$ ******************** Tentara Attack ****************************/
-boolean CheckAttackTentara (Bangunan B, IdxType X, int N) {
-  return (Tentara(ElmtBan(B, X)) >= N);
-}
-
-void TentaraAttack (Bangunan * B, IdxType X, int N) {
-  Tentara(ElmtBan(*B, X)) -= N;
-}
+/* Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak */
 
 
-//$ ******************** Tentara Invaded **************************/
-void InvadedShield (Bangunan * B, IdxType i, int N) {
-  /* KAMUS LOKAL */
 
-  /* ALGORITMA */
-  if (Tentara(ElmtBan(*B, i)) > N * 3 / 4) {
-    Tentara(ElmtBan(*B, i)) = Tentara(ElmtBan(*B, i)) - N * 3 / 4;
-  } else /* Tentara(ElmtBan(*B, i)) <= N * 3 / 4 */ {
-    Tentara(ElmtBan(*B, i)) = (-1) * (N - Tentara(ElmtBan(*B, i)) * 4 / 3);
-  }
-}
 
-void TentaraInvaded (Bangunan * B, boolean Critical_Hit, boolean Attack_Up, boolean Shield, IdxType p, IdxType e, int N) {
-  /* KAMUS LOKAL */
 
-  /* ALGORITMA */
-  if (Critical_Hit) {
-    if (Tentara(ElmtBan(*B, e)) > 2 * N) {
-      Tentara(ElmtBan(*B, e)) = Tentara(ElmtBan(*B, e)) - N * 2;
-      
-    } else /* Tentara(ElmtBan(*B, e)) <= 2 * N */ {
-      if (Tentara(ElmtBan(*B, e)) % 2 == 1) {
-        Tentara(ElmtBan(*B, e)) += 1;
-      }
-      Tentara(ElmtBan(*B, e)) = (-1) * (N - Tentara(ElmtBan(*B, e)) / 2);
+/* ********** OPERASI LAIN ********** */
+void CopyTab(TabInt Tin, TabInt *Tout){
+    IdxType i;
+    MakeEmpty(Tout, MaxEl(Tin));
+    for(i = GetFirstIdx(Tin); i <= GetLastIdx(Tin); i++){
+        Elmt(*Tout,i) = Elmt(Tin, i);
     }
-
-  } else /* !Critical_Hit */ {
-    if (Attack_Up) {
-      TentaraAttack(B, e, N);
-
-    } else /* !Attack_Up */ {
-      if (Shield) {
-        InvadedShield(B, e, N);
-
-      } else /* !Shield */ { //* Skill apapun tidak aktif yang berhubungan dengan Attack
-        if (Name(ElmtBan(*B, e)) == 'T') {
-          InvadedShield(B, e, N);
-        } else if (Name(ElmtBan(*B, e)) == 'F') {
-          if ((Level(ElmtBan(*B, e)) == 3) || (Level(ElmtBan(*B, e)) == 4)) {
-            InvadedShield(B, e, N);
-
-          } else /* (Level(ElmtBan(*B, i)) == 1) || (Level(ElmtBan(*B, i)) == 2) */ {
-            TentaraAttack(B, e, N);
-          }
-
-        } else /* (Name(ElmtBan(*B, i)) == 'C') || (Name(ElmtBan(*B, i)) == 'V') */ {
-          TentaraAttack(B, e, N);
-        }
-      }
-    }
-  }
-  TentaraAttack(B, p, N);
+    Neff(*Tout) = Neff(Tin);
 }
+/* I.S. Tin terdefinisi tidak kosong, Tout sembarang */
+/* F.S. Tout berisi salinan dari Tin (identik, Neff dan MaxEl sama) */
+/* Proses : Menyalin isi Tin ke Tout */
 
-boolean CanCapture (Bangunan B, IdxType i) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  return (Tentara(ElmtBan(B, i)) <= 0);
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void AddAsLastEl(TabInt *T, ElType X){
+    Elmt(*T, GetLastIdx(*T)+1) = X;
+    Neff(*T) += 1;
 }
-
-void TentaraAbsolute (Bangunan * B, IdxType X) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  Tentara(ElmtBan(*B, X)) = abs(Tentara(ElmtBan(*B, X)));
+/* Proses: Menambahkan X sebagai elemen terakhir tabel */
+/* I.S. Tabel T boleh kosong, tetapi tidak penuh */
+/* F.S. X adalah elemen terakhir T yang baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+void DelLastEl(TabInt *T, ElType *X){
+    *X = Elmt(*T,GetLastIdx(*T));
+    Neff(*T) -= 1;
 }
+/* Proses : Menghapus elemen terakhir tabel */
+/* I.S. Tabel tidak kosong */
+/* F.S. X adalah nilai elemen terakhir T sebelum penghapusan, */
+/*      Banyaknya elemen tabel berkurang satu */
+/*      Tabel T mungkin menjadi kosong */
 
-//$ ************************ Level-Up Bangunan ********************************/
-boolean CheckLevelUp (Bangunan B, IdxType X) {
-// ! Jika udah level 4 maka tidak bisa Level Up lagi
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  if (Name(ElmtBan(B, X)) == 'C') {
-    if (Level(ElmtBan(B, X)) == 1) {
-      return (Tentara(ElmtBan(B, X)) >= MaxC1/2);
-
-    } else if (Level(ElmtBan(B, X)) == 2) {
-      return (Tentara(ElmtBan(B, X)) >= MaxC2/2);
-
-    } else if (Level(ElmtBan(B, X)) == 3) {
-      return (Tentara(ElmtBan(B, X)) >= MaxC3/2);
-
-    } else /* Level(ElmtBan(B, X)) == 4 */ {
-      return false;
-
-    }
-  } else if (Name(ElmtBan(B, X)) == 'T') {
-    if (Level(ElmtBan(B, X)) == 1) {
-      return (Tentara(ElmtBan(B, X)) >= MaxT1/2);
-
-    } else if (Level(ElmtBan(B, X)) == 2) {
-      return (Tentara(ElmtBan(B, X)) >= MaxT2/2);
-
-    } else if (Level(ElmtBan(B, X)) == 3) {
-      return (Tentara(ElmtBan(B, X)) >= MaxT3/2);
-
-    } else /* Level(ElmtBan(B, X)) == 4 */ {
-      return false;
-
-    }
-  } else if (Name(ElmtBan(B, X)) == 'F') {
-    if (Level(ElmtBan(B, X)) == 1) {
-      return (Tentara(ElmtBan(B, X)) >= MaxF1/2);
-
-    } else if (Level(ElmtBan(B, X)) == 2) {
-      return (Tentara(ElmtBan(B, X)) >= MaxF2/2);
-
-    } else if (Level(ElmtBan(B, X)) == 3) {
-      return (Tentara(ElmtBan(B, X)) >= MaxF3/2);
-
-    } else /* Level(ElmtBan(B, X)) == 4 */ {
-      return false;
-
-    }
-  } else if (Name(ElmtBan(B, X)) == 'V') /* Name(ElmtBan(B, X)) == 'V' */ {
-    if (Level(ElmtBan(B, X)) == 1) {
-      return (Tentara(ElmtBan(B, X)) >= MaxV1/2);
-
-    } else if (Level(ElmtBan(B, X)) == 2) {
-      return (Tentara(ElmtBan(B, X)) >= MaxV2/2);
-
-    } else if (Level(ElmtBan(B, X)) == 3) {
-      return (Tentara(ElmtBan(B, X)) >= MaxV3/2);
-
-    } else /* Level(ElmtBan(B, X)) == 4 */ {
-      return false;
-
-    }
-  } else {return false;}
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void GrowTab(TabInt *T, int num){
+    int size = MaxEl(*T);
+    TI(*T) = (ElType *) realloc (TI(*T), sizeof(int)*(size+num));
+    MaxEl(*T) += num;
 }
+/* Proses : Menambahkan max element sebanyak num */
+/* I.S. Tabel sudah terdefinisi */
+/* F.S. Ukuran tabel bertambah sebanyak num */
 
-void LevelUp (Bangunan * B, IdxType X) {
-  /* KAMUS LOKAL */
-
-  /* ALGORITMA */
-  Tentara(ElmtBan(*B, X)) -= GetMaxTentara(*B,X)/2;
-  Level(ElmtBan(*B, X)) += 1;
+void ShrinkTab(TabInt *T, int num){
+    int size = MaxEl(*T);
+    TI(*T) = (ElType *) realloc (TI(*T), sizeof(int)*(size-num));
+    MaxEl(*T) -= num;
+    if(Neff(*T) > MaxEl(*T)){
+		Neff(*T) = MaxEl(*T);
+	}
 }
+/* Proses : Mengurangi max element sebanyak num */
+/* I.S. Tabel sudah terdefinisi, ukuran MaxEl > num, dan Neff < MaxEl - num. */
+/* F.S. Ukuran tabel berkurang sebanyak num. */
 
-void ResetLevel (Bangunan * B, IdxType X) {
-  Level(ElmtBan(*B, X)) = 1;
+void CompactTab(TabInt *T){
+    TI(*T) = (ElType *) realloc (TI(*T), (Neff(*T)+1)*sizeof(int));
+    MaxEl(*T) = Neff(*T);
 }
-
-Bangunan CopyBangunan(Bangunan B) {
-  /* KAMUS LOKAL */
-  Bangunan ret;
-  int i;
-
-  /* ALGORITMA */
-  MakeEmptyBangunan(&ret, MaxEl(B));
-  //printf("Maxel :%d  NbElmt: %d\n", MaxEl(B), NbElmtBan(B));
-  for (i = 1; i <= NbElmtBan(B); i++) {
-    Name(ElmtBan(ret, i)) = Name(ElmtBan(B, i));
-    Posisi(ElmtBan(ret, i)) = CopyPOINT(Posisi(ElmtBan(B, i)));
-    Level(ElmtBan(ret, i)) = Level(ElmtBan(B, i));
-    Tentara(ElmtBan(ret, i)) = Tentara(ElmtBan(B, i));
-    Moved(ElmtBan(ret, i)) = Moved(ElmtBan(B, i));
-    Attacked(ElmtBan(ret, i)) = Attacked(ElmtBan(B, i));
-    /*
-    printf("B %d: %c, lv %d, t %d\n", i, Name(ElmtBan(B, i)), Level(ElmtBan(B, i)), Tentara(ElmtBan(B, i)));
-    TulisPOINT(Posisi(ElmtBan(B, i)));
-    puts("");
-    printf("ret %d: %c, lv %d, t %d\n", i, Name(ElmtBan(ret, i)), Level(ElmtBan(ret, i)), Tentara(ElmtBan(ret, i)));
-    TulisPOINT(Posisi(ElmtBan(ret, i)));
-    puts("");
-    */
-  }
-  Neff(ret) = Neff(B);
-  return ret;
-}
+/* Proses : Mengurangi max element sehingga Neff = MaxEl */
+/* I.S. Tabel tidak kosong */
+/* F.S. Ukuran MaxEl = Neff */
